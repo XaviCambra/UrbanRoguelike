5c2725ca7b4ee0969da840bfd57891478da35bce
David Barea <dbareag@edu.tecnocampus.cat>
2023-04-18 19:45:11 +0200

    Input Controller

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -9,4 +9,33 @@ public class InputController : MonoBehaviour
     public KeyCode m_BackKey = KeyCode.S;
     public KeyCode m_LeftKey = KeyCode.A;
     public KeyCode m_RightKey = KeyCode.D;
+    public KeyCode m_Crouch = KeyCode.LeftShift;
+
+
+    public int MouseButton(MouseButton button)
+    {
+        switch (button)
+        {
+            case global::MouseButton.Left:
+                return 0;
+
+            case global::MouseButton.Center:
+                return 2;
+
+            case global::MouseButton.Right:
+                return 1;
+
+            default:
+                return -1;
+        }
+    }
+
+
+}
+
+public enum MouseButton
+{
+    Left = 0,
+    Center = 2,
+    Right = 1
 }
