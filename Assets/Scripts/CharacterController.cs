5dc73c56c56abc42a57a8d92218acb1d1bf9987b
XaviCambra <l4sixx@gmail.com>
2023-04-18 12:27:37 +0200

    CharacterController Created

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public KeyCode m_ForwardKey = KeyCode.W;
+    public KeyCode m_BackKey = KeyCode.S;
+    public KeyCode m_LeftKey = KeyCode.A;
+    public KeyCode m_RightKey = KeyCode.D;
+}
