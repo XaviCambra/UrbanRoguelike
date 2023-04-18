e701a9e74ebfd47c4416478b72f2cd466cea18b1
XaviCambra <l4sixx@gmail.com>
2023-04-18 21:46:14 +0200

    Movimiento corregido

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,20 +27,22 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKey(m_InputController.m_ForwardKey))
         {
-            l_Direction += Vector3.forward;
+            l_Direction += Quaternion.Euler(0, 45, 0) * Vector3.forward;
         }
         if (Input.GetKey(m_InputController.m_BackKey))
         {
-            l_Direction += Vector3.back;
+            l_Direction += Quaternion.Euler(0, 45, 0) * Vector3.back;
         }
         if (Input.GetKey(m_InputController.m_LeftKey))
         {
-            l_Direction += Vector3.left;
+            l_Direction += Quaternion.Euler(0, 45, 0) * Vector3.left;
         }
         if (Input.GetKey(m_InputController.m_RightKey))
         {
-            l_Direction += Vector3.right;
+            l_Direction += Quaternion.Euler(0, 45, 0) * Vector3.right;
         }
+
+        l_Direction.Normalize();
 
         m_CharacterController.Move(l_Direction * m_Blackboard.m_MovementSpeed * Time.deltaTime);
     }
