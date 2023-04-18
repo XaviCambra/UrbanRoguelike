ac2f07f7eb9818ce4ebb44d7f97fa7aed503a976
XaviCambra <l4sixx@gmail.com>
2023-04-18 13:37:18 +0200

    Inputs moved to InputController

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    [Header("Inputs")]
-    public KeyCode m_ForwardKey = KeyCode.W;
-    public KeyCode m_BackKey = KeyCode.S;
-    public KeyCode m_LeftKey = KeyCode.A;
-    public KeyCode m_RightKey = KeyCode.D;
+    void MovementInput()
+    {
+
+    }
 
 
 }
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [Header("Inputs")]
+    public KeyCode m_ForwardKey = KeyCode.W;
+    public KeyCode m_BackKey = KeyCode.S;
+    public KeyCode m_LeftKey = KeyCode.A;
+    public KeyCode m_RightKey = KeyCode.D;
+}
