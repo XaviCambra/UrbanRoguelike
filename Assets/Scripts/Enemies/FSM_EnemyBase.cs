e3ca0f8775373ce9773a875af7693a7882111bca
XaviCambra <l4sixx@gmail.com>
2023-04-21 12:15:17 +0200

    Limpiar FSM EnemyBase

--- Assets/Scripts/Enemies/FSM_EnemyBase.cs
+++ Assets/Scripts/Enemies/FSM_EnemyBase.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class FSM_EnemyBase : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
