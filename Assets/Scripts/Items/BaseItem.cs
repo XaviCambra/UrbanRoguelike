226be0e5a3706c55d48867581e4b785a1a8ff6b9
David Barea <dbareag@edu.tecnocampus.cat>
2023-04-21 18:20:24 +0200

    Cambios Script Key y BaseItem

--- Assets/Scripts/Items/BaseItem.cs
+++ Assets/Scripts/Items/BaseItem.cs
@@ -4,7 +4,18 @@ public class BaseItem : MonoBehaviour
 {
     public virtual void ApplyEffectItem()
     {
-        PlayerController playerController = GetComponent<PlayerController>();
+        PlayerController playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         playerController.m_Item = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController playerController = other.GetComponent<PlayerController>();
+
+        if (playerController == null) return;
+
+        playerController.GetComponent<PlayerController>().m_Item = this;
+
+        gameObject.SetActive(false);
     }
 }
