d02dca124ae861f730574ccdd0f8b4ecc606d266
David Barea <dbareag@edu.tecnocampus.cat>
2023-04-24 12:29:48 +0200

    Modificado prefab y script de llave

--- Assets/Scripts/Items/KeyItem.cs
+++ Assets/Scripts/Items/KeyItem.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class KeyItem : BaseItem
 {
+    [SerializeField] private float m_DetectionRadius;
 
     public override void ApplyEffectItem()
     {
 
         /*  Write your own code below */
 
+        Collider[] l_colliders = Physics.OverlapSphere(transform.position, m_DetectionRadius);
+        
+        foreach (Collider l_nearbyObject in l_colliders)
+        {
+            if (l_nearbyObject.tag == "OpenDoorTrigger")
+            {
+                Debug.Log("Door Trigger Detected");
+                DoorTriggerController l_DoorTrigger = l_nearbyObject.GetComponent<DoorTriggerController>();
 
-        base.ApplyEffectItem();
+                l_DoorTrigger.OpenDoor();
+
+                Debug.Log("KeyUsed");
+                Destroy(gameObject);
+            }
+
+            else return;
+        }
     }
 }
