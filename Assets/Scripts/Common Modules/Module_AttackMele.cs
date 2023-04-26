4a168cf128ef3fa3f1a41ffd0361c8fe5ab3c6ec
David Barea <dbareag@edu.tecnocampus.cat>
2023-04-26 16:49:08 +0200

    Cambios en el melee enemy

--- Assets/Scripts/Common Modules/Module_AttackMele.cs
+++ Assets/Scripts/Common Modules/Module_AttackMele.cs
@@ -2,26 +2,41 @@ using UnityEngine;
 
 public class Module_AttackMele : MonoBehaviour
 {
-    public GameObject m_HitPoint;
+    public GameObject m_EnemyFist;
     float m_Damage;
-    Module_Animation m_Animation;
+    //Module_Animation m_Animation;
+    public Animator m_animator;
+
+    BoxCollider m_FistCollider;
 
     public void HitOnDirection(float l_Damage)
     {
         m_Damage = l_Damage;
 
-        if (m_Animation == null) return;
-        m_Animation.PlayAnimation("MeleAttack");
+        /*if (m_Animation == null) return;
+        m_Animation.PlayAnimation("MeleAttack");*/
+
+        if (m_animator == null) return;
+        m_animator.SetTrigger("MeleAttack");
+
     }
 
     public void ActivateCollisionDetection()
     {
-        m_HitPoint.SetActive(true);
+        m_FistCollider = GameObject.FindGameObjectWithTag("EnemyFist").GetComponent<BoxCollider>();
+
+        m_FistCollider.enabled = true;
+
+        //m_EnemyFist.SetActive(true);
     }
 
     public void DeactivateCollisionDetection()
     {
-        m_HitPoint.SetActive(false);
+        m_FistCollider = GameObject.FindGameObjectWithTag("EnemyFist").GetComponent<BoxCollider>();
+
+        m_FistCollider.enabled = false;
+
+        //m_EnemyFist.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
