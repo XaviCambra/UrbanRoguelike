9f4c7ead1011033d3ec1a5147c9800e1d36760c5
XaviCambra <l4sixx@gmail.com>
2023-05-06 20:36:18 +0200

    Room Generator

---
+++ Assets/Editor/RoomGeneratorEditor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RoomGenerator))]
+public class RoomGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RoomGenerator m_RoomGenerator = (RoomGenerator)target;
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Generate Room Testing Button"))
+        {
+            m_RoomGenerator.GenerateRandomScene();
+        }
+    }
+}
--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -5,8 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class RoomGenerator : MonoBehaviour
 {
+    public float m_RoomOffset = 20;
+    public int m_IndexRoomsGenerated = 0;
+
+    private void Start()
+    {
+        GenerateRandomScene();
+    }
     public void GenerateRandomScene()
     {
-        SceneManager.LoadScene((int)Random.Range(0, 1), LoadSceneMode.Additive);
+        int RandomIndexScene = (int)Random.Range(0, 2);
+        SceneManager.LoadScene(RandomIndexScene, LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneAt(RandomIndexScene));
+        GameObject[] l_Rooms = GameObject.FindGameObjectsWithTag("MainMap");
+        m_IndexRoomsGenerated = l_Rooms.Length;
+        foreach (GameObject RoomFound in l_Rooms)
+        {
+            SceneController l_SceneController = RoomFound.GetComponent<SceneController>();
+            if (l_SceneController == null) return;
+            if (l_SceneController.m_SettedRoom) return;
+
+            Vector3 l_MapPosition = Vector3.right * m_RoomOffset * m_IndexRoomsGenerated;
+            RoomFound.transform.position = l_MapPosition;
+
+            l_SceneController.m_SettedRoom = true;
+        }
     }
 }
