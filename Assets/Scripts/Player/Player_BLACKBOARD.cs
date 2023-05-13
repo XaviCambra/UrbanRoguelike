7069904eb7c8753721ff747db1724d2829ff0acf
Xavi-Develop <l4sixx@gmail.com>
2023-05-13 16:59:22 +0200

    Limpieza de codigo

--- Assets/Scripts/Player/Player_BLACKBOARD.cs
+++ Assets/Scripts/Player/Player_BLACKBOARD.cs
@@ -10,30 +10,34 @@ public class Player_BLACKBOARD : MonoBehaviour
     [SerializeField] private const float m_BASEShootingDamage = 10.0f;
     [SerializeField] private const float m_BASEReloadSpeed = 3.0f;
     [SerializeField] private const float m_BASEBulletSpeed = 10.0f;
+    [SerializeField] private const float m_BASEDashDistance = 1.5f;
+    [SerializeField] private const float m_BASEDashSpeed = 8.0f;
     [SerializeField] private const int m_BASEMaxOverHeat = 5;
-    [SerializeField] private const int m_BaseOverHeatWindow = 3;
 
-    [Header("Variable Movement")]
+    [Header("Movement")]
     public float m_MovementSpeed;
     public float m_CrouchingSpeed;
 
-    [Header("Variable Shoot")]
+    [Header("Shoot")]
+    public bool m_CanAttack;
     public float m_ShootingDamage;
     public float m_BulletSpeed;
     public float m_ReloadSpeed;
     public float m_MaxOverHeat;
-    public float m_OverHeatWindow;
-    public float m_InmortalityDuration;
-    public Transform m_ShootPoint;
-    public bool m_CanAttack;
-
     public float m_OverHeatCancelDuration;
+    public Transform m_ShootPoint;
 
+    [Header("Dash")]
+    public float m_DashDistance;
+    public float m_DashSpeed;
+
+    [Header("Health")]
+    public float m_InmortalityDuration;
+
+    [Header("Items")]
     public BaseItem m_Item;
     public PowerUp_Base m_PowerUp;
 
-
-    public bool m_HasKey;
 
     private void Start()
     {
@@ -48,6 +52,5 @@ public class Player_BLACKBOARD : MonoBehaviour
         m_ReloadSpeed = m_BASEReloadSpeed;
         m_BulletSpeed = m_BASEBulletSpeed;
         m_MaxOverHeat = m_BASEMaxOverHeat;
-        m_OverHeatWindow = m_BaseOverHeatWindow;
     }
 }
