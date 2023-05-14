76591049a4fc066d0be7433e6a0254aab68338ba
Xavi-Develop <l4sixx@gmail.com>
2023-05-14 22:42:44 +0200

    Tienda fixed - Cambios enemy Mele, cambios camara, cambios menu

--- Assets/Scripts/Shop/PickeableCard.cs
+++ Assets/Scripts/Shop/PickeableCard.cs
@@ -9,5 +9,6 @@ public class PickeableCard : MonoBehaviour
     public void SetActiveCard()
     {
         m_ActiveCard.SetPower(GetComponent<PowerUpCard>().m_PowerUp);
+        m_ActiveCard.m_CardIndex = GetComponent<PowerUpCard>().m_CardIndex;
     }
 }
