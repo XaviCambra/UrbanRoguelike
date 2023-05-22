01058e71c915f7c0705381475d4ce77255236a28
XaviCambra <l4sixx@gmail.com>
2023-05-22 19:43:41 +0200

    4 bugs en 1 commit

--- Assets/Scripts/Enemies/RangedEnemy.cs
+++ Assets/Scripts/Enemies/RangedEnemy.cs
@@ -10,13 +10,18 @@ public class RangedEnemy : FSM_EnemyBase
     Module_Crouch m_Crouch;
     GameObject m_PlayerHitpoint;
 
-    //EnemyBase_BLACKBOARD m_BlackBoard;
-
     public GameObject m_Player;
-
 
     [SerializeField] private GameObject m_GrenadePrefab;
     [SerializeField] private float m_GrenadeForce;
+
+    private enum AttackType
+    {
+        Bullet,
+        Grenade
+    }
+
+    private AttackType m_AttackType;
 
     void Start()
     {
@@ -44,56 +49,49 @@ public class RangedEnemy : FSM_EnemyBase
             SetStateAttack();
     }
 
-    protected override void SetStateMovement()
-    {
-        base.SetStateMovement();
-        Vector3 l_LookAtPlayer = m_Player.transform.position;
-        l_LookAtPlayer.y = transform.position.y;
-        transform.LookAt(l_LookAtPlayer);
-    }
-
     protected override void SetStateAttack()
     {
+        CrouchOut();
         base.SetStateAttack();
-        if (m_Blackboard.m_CanAttack == false) return;
+    }
 
-        if (Vector3.Distance(m_Player.transform.position, transform.position) < m_Blackboard.m_AttackDistance)
+    public override void StateAttack()
+    {
+        base.StateAttack();
+
+        if (!m_Blackboard.m_CanAttack)
+            return;
+
+        switch (m_AttackType)
         {
-            m_AttackRanged.ShootOnDirection(m_Blackboard.m_AttackPoint.position, m_Blackboard.m_AttackPoint.transform.rotation, m_Blackboard.m_AttackSpeed, m_Blackboard.m_Damage, "Enemy");
+            case AttackType.Bullet:
+                m_AttackRanged.ShootOnDirection(m_Blackboard.m_AttackPoint.position, m_Blackboard.m_AttackPoint.transform.rotation, m_Blackboard.m_AttackSpeed, m_Blackboard.m_Damage, "Enemy");
+                break;
+            case AttackType.Grenade:
+                GameObject l_grenade = Instantiate(m_GrenadePrefab, m_Blackboard.m_AttackPoint.transform.position, m_Blackboard.m_AttackPoint.transform.rotation);
+                Rigidbody l_rb = l_grenade.GetComponent<Rigidbody>();
+                l_rb.AddForce(m_Blackboard.m_AttackPoint.transform.forward * m_GrenadeForce, ForceMode.VelocityChange);
+                l_grenade.SetActive(true);
+                break;
+            default:
+                break;
         }
-
-        if (Vector3.Distance(m_Player.transform.position, transform.position) < m_Blackboard.m_GrenadeDistance)
-        {
-            GameObject l_grenade = Instantiate(m_GrenadePrefab, m_Blackboard.m_AttackPoint.transform.position, m_Blackboard.m_AttackPoint.transform.rotation);
-            Rigidbody l_rb = l_grenade.GetComponent<Rigidbody>();
-            l_rb.AddForce(m_Blackboard.m_AttackPoint.transform.forward * m_GrenadeForce, ForceMode.VelocityChange);
-            l_grenade.SetActive(true);
-        }
-
         m_Blackboard.m_CanAttack = false;
         StartCoroutine(CrouchIn());
+
+    }
+
+    private IEnumerator CrouchOut()
+    {
+        yield return new WaitForSeconds(1.0f);
+        m_Crouch.AlternateCrouching(true);
+        m_Blackboard.m_CanAttack = true;
     }
 
     private IEnumerator CrouchIn()
     {
         yield return new WaitForSeconds(3.0f);
-        Debug.Log("Crouch");
         m_Crouch.AlternateCrouching(false);
-        StartCoroutine(RechargeAttack());
+        SetStateWait(m_Blackboard.m_AttackCooldown);
     }
-    private IEnumerator CrouchOut()
-    {
-        yield return new WaitForSeconds(1.0f);
-        Debug.Log("Crouch Out");
-        m_Crouch.AlternateCrouching(true);
-        m_Blackboard.m_CanAttack = true;
-    }
-
-    protected IEnumerator RechargeAttack()
-    {
-        float l_RechargeAttack = m_Blackboard.m_AttackCooldown + Random.Range(-0.75f, 0.75f);
-        yield return new WaitForSeconds(l_RechargeAttack);
-        StartCoroutine(CrouchOut());
-    }
-
 }
