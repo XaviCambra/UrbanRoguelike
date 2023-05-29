d0cdfa2b50d443eeaedff4f3126fd2423ccddbf4
XaviCambra <l4sixx@gmail.com>
2023-05-29 17:42:38 +0200

    Limpieza carpetas auto-generated

---
+++ Assets/Scripts/Traps & Doors/CloseDoor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoor : BaseItem
+{
+    public override void ApplyEffectItem()
+    {
+        base.ApplyEffectItem();
+
+        /*  Write your own code below */
+
+    }
+}
