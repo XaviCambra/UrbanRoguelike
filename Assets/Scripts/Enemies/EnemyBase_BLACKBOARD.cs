7cae32b4743906bcde0ae08331d6c66f33995ff5
Xavi-Develop <l4sixx@gmail.com>
2023-05-30 12:54:22 +0200

    Boss Room Trigger, enemy blackboard edited

--- Assets/Scripts/Enemies/EnemyBase_BLACKBOARD.cs
+++ Assets/Scripts/Enemies/EnemyBase_BLACKBOARD.cs
@@ -4,43 +4,39 @@ using UnityEngine;
 
 public class EnemyBase_BLACKBOARD : MonoBehaviour
 {
+    [Header("Common")]
     [Header("Movement")]
     public float m_WalkSpeed;
     public float m_RunSpeed;
-
+    [Header("Distances")]
+    public float m_DetectionRadius;
+    [Header("Other variables")]
+    public bool m_CanAttack;
+    public bool m_IsActive = true;
+    [Header("Objects")]
+    public Transform m_AttackPoint;
+    [Header("Score Points")]
+    public int m_Points;
+    [Header("Mele Enemy")]
     [Header("Dash")]
     public float m_DashDistance;
     public float m_DashSpeed;
     public float m_DashChargedDistance;
-
+    [Header("Distances")]
+    public float m_FollowDistance;
+    public float m_AttackDistance;
+    [Header("Ranged Enemy")]
     [Header("Crouching")]
     public bool m_Crouching;
     public float m_CrouchInTime;
     public float m_CrouchOutTime;
-
     [Header("Bullet")]
     public float m_Damage;
     public float m_AttackSpeed;
     public float m_AttackCooldown;
-
     [Header("Grenade")]
     public bool m_GrenadeLoaded = true;
     public float m_GrenadeDistance;
     public float m_GrenadeCooldown;
     public float m_GrenadeForce;
-
-    [Header("Distances")]
-    public float m_DetectionRadius;
-    public float m_FollowDistance;
-    public float m_AttackDistance;
-
-    [Header("Other variables")]
-    public bool m_CanAttack;
-    public bool m_IsActive = true;
-
-    [Header("Objects")]
-    public Transform m_AttackPoint;
-
-    [Header("Score Points")]
-    public int m_Points;
 }
--- Assets/Scripts/Enemies/FinalBossEnemy.cs
+++ Assets/Scripts/Enemies/FinalBossEnemy.cs
@@ -7,7 +7,17 @@ public class FinalBossEnemy : FSM_EnemyBase
     Module_AttackRanged m_AttackRanged;
     GameObject m_PlayerHitpoint;
     LineRenderer m_LineRenderer;
+    
+    [SerializeField] private GameObject m_GrenadePrefab;
+    [SerializeField] private float m_GrenadeForce;
 
+    private enum AttackType
+    {
+        Bullet,
+        Grenade,
+        Misile
+    }
+    [SerializeField] private AttackType m_AttackType;
     void Start()
     {
         m_Blackboard = GetComponent<EnemyBase_BLACKBOARD>();
@@ -21,7 +31,43 @@ public class FinalBossEnemy : FSM_EnemyBase
     protected override void Update()
     {
         base.Update();
+        m_LineRenderer.SetPosition(0, m_Blackboard.m_AttackPoint.position);
+        m_LineRenderer.SetPosition(1, m_PlayerHitpoint.transform.position);
+    }
 
+    protected override void SetStateIdle()
+    {
+        m_State = EnemyStates.Attack;
+    }
 
+    public override void StateAttack()
+    {
+        base.StateAttack();
+
+        if (!m_Blackboard.m_CanAttack)
+            return;
+
+        Debug.Log("Dispara");
+        switch (m_AttackType)
+        {
+            case AttackType.Bullet:
+                m_AttackRanged.ShootOnDirection(m_Blackboard.m_AttackPoint.position, m_Blackboard.m_AttackPoint.transform.rotation, m_Blackboard.m_AttackSpeed, m_Blackboard.m_Damage, "Enemy");
+                break;
+            case AttackType.Grenade:
+                GameObject l_grenade = Instantiate(m_GrenadePrefab, m_Blackboard.m_AttackPoint.transform.position, m_Blackboard.m_AttackPoint.transform.rotation);
+                Rigidbody l_rb = l_grenade.GetComponent<Rigidbody>();
+                l_rb.AddForce(m_Blackboard.m_AttackPoint.transform.forward * m_GrenadeForce, ForceMode.VelocityChange);
+                l_rb.useGravity = true;
+                l_grenade.SetActive(true);
+                m_Blackboard.m_GrenadeLoaded = false;
+                //StartCoroutine(GrenadeCooldown());
+                break;
+            case AttackType.Misile:
+                break;
+            default:
+                break;
+        }
+        m_Blackboard.m_CanAttack = false;
+        SetStateWait(m_Blackboard.m_AttackCooldown + 3);
     }
 }
