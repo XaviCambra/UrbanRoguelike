d2377b7ba4d0e66b8c6f3ec9ef1f5517c8e16704
XaviCambra <l4sixx@gmail.com>
2023-06-12 19:53:35 +0200

    Loading Screen Updated

--- Assets/Scripts/UI/UI_Loading.cs
+++ Assets/Scripts/UI/UI_Loading.cs
@@ -7,6 +7,7 @@ public class UI_Loading : MonoBehaviour
     public GameObject m_PlayerHud;
 
     public TextMeshProUGUI m_LoadingText;
+    public Slider m_LoadingSlider;
 
     float m_Time = 0;
     float m_AlphaPanel = 1;
@@ -14,6 +15,7 @@ public class UI_Loading : MonoBehaviour
     private void Start()
     {
         m_PlayerHud.SetActive(false);
+        m_LoadingText.text = "Loading...";
     }
 
     // Update is called once per frame
@@ -21,17 +23,13 @@ public class UI_Loading : MonoBehaviour
     {
         m_Time += Time.deltaTime;
 
-        string l_LoadingText = "Loading";
-        for (int i = 0; i < m_Time; i++)
-        {
-            l_LoadingText += ".";
-        }
-        m_LoadingText.text = l_LoadingText;
+        m_LoadingSlider.value = m_Time / 3;
 
         if(m_Time > 3)
         {
             m_PlayerHud.SetActive(true);
             m_LoadingText.gameObject.SetActive(false);
+            m_LoadingSlider.gameObject.SetActive(false);
             GetComponent<Image>().color = new Color(0, 0, 0, m_AlphaPanel);
             m_AlphaPanel -= Time.deltaTime;
             if (m_AlphaPanel < 0)
