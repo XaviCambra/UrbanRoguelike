7eb6f78db93e3157c4f433c768891c3ea5280ea2
David Barea <dbareag@edu.tecnocampus.cat>
2023-06-14 13:05:47 +0200

    Dash UI actualizada

--- Assets/Scripts/UI/UI_PlayerDash.cs
+++ Assets/Scripts/UI/UI_PlayerDash.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class UI_PlayerDash : MonoBehaviour
 {
     Player_BLACKBOARD m_PlayerBlackboard;
-    //Slider m_Slider;
-    public Image DashImage1;
-    public Image DashImage2;
+
+    public List<Image> DashImageList;
 
     // Start is called before the first frame update
     void Start()
     {
         m_PlayerBlackboard = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_BLACKBOARD>();
-        //m_Slider = GetComponent<Slider>();
+
+        for (int i = 0; i < DashImageList.Count; i++)
+        {
+            DashImageList[i].enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -22,35 +25,20 @@ public class UI_PlayerDash : MonoBehaviour
     {
         if (m_PlayerBlackboard == null)
             return;
-        /*if (m_Slider == null)
-            return;
-        m_Slider.maxValue = m_PlayerBlackboard.m_DashMaxCount;
-        m_Slider.value = m_PlayerBlackboard.m_DashMaxCount - m_PlayerBlackboard.m_DashCount;*/
 
         updateDashImages();
     }
 
     void updateDashImages()
     {
-        if (m_PlayerBlackboard.m_DashCount == 0)
+        for (int i = 0; i < m_PlayerBlackboard.m_DashMaxCount; i++)
         {
-            DashImage1.enabled = true;
-
-            DashImage2.enabled = true;
+            DashImageList[i].enabled = true;
         }
 
-        if (m_PlayerBlackboard.m_DashCount == 1)
+        for (int i = 0; i < m_PlayerBlackboard.m_DashCount; i++)
         {
-            DashImage1.enabled = true;
-
-            DashImage2.enabled = false;
-        }
-
-        if (m_PlayerBlackboard.m_DashCount == 2)
-        {
-            DashImage1.enabled = false;
-
-            DashImage2.enabled = false;
+            DashImageList[i].enabled = false;
         }
     }
 }
